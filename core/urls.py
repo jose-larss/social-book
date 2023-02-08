b8c2d2133833e6d66407d91bc1b486c78cdc82b5
jlcastro <joselucianocastro10@gmail.com>
2023-02-08 12:26:35 +0100

    realizado signin, signup y logout

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from .import views #haciendolo asi no hae falta especificar nombres de funciones
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('signup/',views.signup, name="signup"),
+    path('signin/',views.signin, name="signin"),
+    path('logout/',views.logout, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,74 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.shortcuts import render, redirect
 
 
+@login_required(login_url="signin")
 def index(request):
     return render(request, "index.html")
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Taken (Ya cogido)")
+                return redirect("signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username Taken (Ya cogido)")
+                return redirect("signup")
+            else:
+                user = User.objects.create_user(
+                    username=username, 
+                    email=email, 
+                    password=password
+                )
+                user.save() #ya tenemos el usuario creado
+                #log user in and redirect to settings page
+
+                #ahora que tenemos creado un usuario, queremos crear un profile para el nuevo usuario "POR DEFECTO"
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                #redirigir al usuario a la pagina de inicio de sesion, pero todavia no la tenemos
+                return redirect("signup")
+
+        else:
+            messages.info(request, "Password NOT Matching")
+            return redirect('signup')
+    else:
+
+        return render(request, 'signup.html')
+
+
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+        print(user)
+
+        if user is not None: #es lo mismo que (user != None)
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Credentials Invalid")
+            return redirect("signin")
+
+    else:
+        return render(request, "signin.html")
+
+
+@login_required(login_url="signin")
+def logout(request):
+    auth.logout(request)
+    return redirect("signin")
